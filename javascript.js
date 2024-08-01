d81e03cd57b7771a840b0b41ed8105306ab52b65
pnaruka054 <pnaruka054@gmail.com>
2024-08-01 09:17:45 +0530

    last commit

--- javascript.js
+++ javascript.js
@@ -3,9 +3,34 @@ let p = document.querySelector('.p');
 let p2 = document.querySelector('.p2');
 
 function addImplicitMultiplication(str) {
-    return str.replace(/(\d)(\()/g, '$1*$2')
+    str = str
+        .replace(/(\d)√(\d+)/g, '$1*Math.sqrt($2)')
+        .replace(/√(\d+)/g, 'Math.sqrt($1)')
+        .replace(/(\d)∛(\d+)/g, '$1*Math.cbrt($2)')
+        .replace(/∛(\d+)/g, 'Math.sbrt($1)')
+        .replace(/(\d)(\()/g, '$1*$2')
         .replace(/(\))(\d)/g, '$1*$2')
-        .replace(/(\))(\()/g, '$1*$2');
+        .replace(/(\))(\()/g, '$1*$2')
+        .replace(/(\d)\!/g, `
+            function factorial(n) {
+                    if (n === 0) {
+                        return 1;
+                    }
+                    return n * factorial(n - 1);
+                }
+                factorial($1)
+                    `);
+    return str;
+}
+
+function evaluateExpression(expression) {
+    let updatedExpression = expression.replace(/\^/g, '**');
+    try {
+        return eval(updatedExpression);
+    } catch (e) {
+        console.error('Error evaluating expression:', e);
+        return '';
+    }
 }
 
 function callback(event) {
@@ -13,29 +38,23 @@ function callback(event) {
     let str_values = '';
 
     switch (true) {
-        case !isNaN(element_value) || ['+', '-', '*', '/', '%', '**', '***', '(', ')', '.'].includes(element_value):
+        case !isNaN(element_value) || ['+', '-', '*', '/', '%', '**', '***', '(', ')', '.', '√', '∛', '!'].includes(element_value):
             if (element_value === '**') {
                 p.innerHTML += '^2';
-                str_values = addImplicitMultiplication(p.innerHTML).replace(/\^/g, '**');
-                p2.innerHTML = eval(str_values);
-                return;
             } else if (element_value === '***') {
                 p.innerHTML += '^3';
-                str_values = addImplicitMultiplication(p.innerHTML).replace(/\^/g, '**');
-                p2.innerHTML = eval(str_values);
-                return;
+            } else if (element_value === '√') {
+                p.innerHTML += '√';
+            } else if (element_value === '∛') {
+                p.innerHTML += '∛';
+            } else if (element_value === '!') {
+                p.innerHTML += '!';
+            } else {
+                p.innerHTML += element_value;
             }
-
-            p.innerHTML += element_value;
-
-            if (p.innerHTML.search(/\^/) !== -1) {
-                let updatedString = p.innerHTML.replace(/\^/g, '**');
-                return p2.innerHTML = eval(updatedString);
-            }
-
 
             str_values = addImplicitMultiplication(p.innerHTML);
-            p2.innerHTML = eval(str_values);
+            p2.innerHTML = evaluateExpression(str_values);
             break;
 
         case element_value === 'clear':
@@ -45,21 +64,13 @@ function callback(event) {
 
         case event.target.classList.contains('backSpace'):
             p.innerHTML = p.innerHTML.slice(0, -1);
-            if (p.innerHTML === '^') {
-                str_values = addImplicitMultiplication(p.innerHTML).replace(/\^/g, '**');
-                p2.innerHTML = eval(str_values);
-                return;
-            }
             str_values = addImplicitMultiplication(p.innerHTML);
-            if(p.innerHTML == ''){
-                return p2.innerHTML = ''
-            }
-            p2.innerHTML = eval(str_values);
+            p2.innerHTML = evaluateExpression(str_values);
             break;
 
         case element_value === '=':
             p.innerHTML = p2.innerHTML;
-            p2.innerHTML = ''
+            p2.innerHTML = '';
             break;
     }
 }
